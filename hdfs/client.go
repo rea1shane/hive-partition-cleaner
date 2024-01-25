0f50c8b30077f2d63d8e5ac637593d8d8cd9f8c0
Shane <innon.shen@gmail.com>
2024-01-25 18:12:00 +0800

    HDFS delete partitions

--- hdfs/client.go
+++ hdfs/client.go
@@ -1,7 +1,11 @@
 package hdfs
 
 import (
+	"errors"
+	"fmt"
 	"github.com/colinmarc/hdfs"
+	"regexp"
+	"strings"
 )
 
 type Client struct {
@@ -25,9 +29,14 @@ func (c *Client) Close() error {
 	return c.client.Close()
 }
 
-// ListPartitions 列出指定表存储路径下的所有分区
+// ListPartitions 列出指定表的存储路径下的所有分区
 func (c *Client) ListPartitions(path string) (partitions []string, err error) {
-	files, err := c.client.ReadDir(path)
+	formattedPath, err := formatPath(path)
+	if err != nil {
+		return
+	}
+
+	files, err := c.client.ReadDir(formattedPath)
 	if err != nil {
 		return
 	}
@@ -37,4 +46,39 @@ func (c *Client) ListPartitions(path string) (partitions []string, err error) {
 		}
 	}
 	return
+}
+
+// DeletePartitions 删除指定表的存储路径下的指定分区
+func (c *Client) DeletePartitions(path string, partitions []string) error {
+	formattedPath, err := formatPath(path)
+	if err != nil {
+		return err
+	}
+
+	for _, partition := range partitions {
+		err = c.client.Remove(fmt.Sprintf("%s/%s", formattedPath, partition))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// formatPath 格式化路径
+// 如果传入的路径为 / 开头，则直接返回
+// 如果传入的路径为 hdfs://{cluster}/{path}，则返回 /{path} 部份
+// 如果传入的路径不符合上述所有的规则，则返回非法路径错误
+func formatPath(path string) (string, error) {
+	if !strings.HasPrefix(path, "hdfs://") && strings.HasPrefix(path, "/") {
+		return path, nil
+	}
+
+	pattern := `hdfs://[^/]*(/.*)`
+	re := regexp.MustCompile(pattern)
+	matches := re.FindStringSubmatch(path)
+	if len(matches) >= 2 {
+		return matches[1], nil
+	} else {
+		return "", errors.New("illegal path")
+	}
 }
--- hdfs/client_test.go
+++ hdfs/client_test.go
@@ -9,7 +9,11 @@ import (
 const (
 	address  = "localhost:8020"
 	username = ""
-	path     = "/user/hive/warehouse/test"
+	path     = "hdfs://namenode:8020/user/hive/warehouse/test"
+)
+
+var (
+	targetPartitions = []string{"`date`=20231111", "`date`=20231112"}
 )
 
 func TestNewClient(t *testing.T) {
@@ -39,9 +43,31 @@ func TestClient_ListPartitions(t *testing.T) {
 		panic(err)
 	}
 	defer client.Close()
+
 	partitions, err := client.ListPartitions(path)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(partitions)
+}
+
+func TestClient_DeletePartitions(t *testing.T) {
+	client, err := NewClient(username, address)
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
+
+	err = client.DeletePartitions(path, targetPartitions)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestFormatPath(t *testing.T) {
+	res, err := formatPath(path)
+	if err != nil {
+		panic(err)
+	}
+	println(res)
 }
